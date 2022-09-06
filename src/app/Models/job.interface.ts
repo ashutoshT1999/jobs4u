0418321fca115eff8b443b6385702eb2015b2ed9
SagarRSingh <sagar.r.singh@globallogic.com>
2022-09-06 10:00:45 +0530

    updated Job Interface

--- src/app/Models/job.interface.ts
+++ src/app/Models/job.interface.ts
@@ -1,3 +1,13 @@
 export interface IJob{
-  
+  id:number;
+  title:string;
+  location:string;
+  designation?:string;
+  description:string;
+  category:string;
+  dateOfPosting:string;
+  isClosed:boolean;
+  ctc:number;
+  lastDateToApply:string;
+  experienceRequired:number;
 }
